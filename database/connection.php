47de992adc56af829757745283677422feca3722
Syffaaa <syiffaasmrydii@gmail.com>
2024-05-16 18:56:51 +0800

    Update connection.php

--- database/connection.php
+++ database/connection.php
@@ -4,7 +4,7 @@ $username = "root";
 $password = "";
 $dbname = "penggajian";
 
-$connection = mysqli_connect{$servername, $username, $password, $dbname};
+$connection = mysqli_connect($servername, $username, $password, $dbname);
 if (!$connection) {
     die("Gagal Koneksi: " . mysqli_connect_error());
 }
